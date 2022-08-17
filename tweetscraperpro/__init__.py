e94747ee06c9199249b6c3a03b3403ca9fdb1a0c
021·Tanko <aisynclab@gmail.com>
2022-08-18 04:17:24 +0800

    Update __init__.py

--- tweetscraperpro/__init__.py
+++ tweetscraperpro/__init__.py
@@ -1,5 +1,15 @@
+'''
+TWEETSCRAPERPRO - TweetScraper Pro (formerly known as TweetSpy).
+
+See wiki on Github for in-depth details.
+https://github.com/haccer/tweetscraperpro/wiki
+
+Licensed under MIT License
+Copyright (c) 2022 021 Tanko
+'''
 from .followers import Followers
 from .following import Following
 from .favorites import Favorites
+from .profile import Profile
 from .search import Search
 from .config import Config
