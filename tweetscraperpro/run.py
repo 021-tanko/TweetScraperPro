6b8d3ba7fba3b74281922c390075ff53e75ec55a
021·Tanko <aisynclab@gmail.com>
2023-01-23 16:32:25 +0800

    Added loggin options, fixes retweets

--- tweetscraperpro/run.py
+++ tweetscraperpro/run.py
@@ -1,10 +1,13 @@
 from . import datelock, feed, get, output, verbose, storage
 from asyncio import get_event_loop
-from datetime import timedelta
+from datetime import timedelta, datetime
 from .storage import db
+
+#import logging
 
 class TweetScraperPro:
     def __init__(self, config):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+__init__')
         if config.Resume is not None and config.TwitterSearch:
             self.init = f"TWEET-{config.Resume}-0"
         else:
@@ -29,6 +32,7 @@ class TweetScraperPro:
                 self.config.Timedelta = (self.d._until - self.d._since).days
 
     async def Feed(self):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+Feed')
         response = await get.RequestUrl(self.config, self.init)
         if self.config.Debug:
             print(response, file=open("tweetscraperpro-last-request.log", "w", encoding="utf-8"))
@@ -50,6 +54,7 @@ class TweetScraperPro:
             pass
 
     async def follow(self):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+follow')
         await self.Feed()
         if self.config.User_full:
             self.count += await get.Multi(self.feed, self.config, self.conn)
@@ -60,10 +65,12 @@ class TweetScraperPro:
                 await output.Username(username, self.config, self.conn)
 
     async def favorite(self):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+favorite')
         await self.Feed()
         self.count += await get.Multi(self.feed, self.config, self.conn)
 
     async def profile(self):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+profile')
         await self.Feed()
         if self.config.Profile_full:
             self.count += await get.Multi(self.feed, self.config, self.conn)
@@ -73,6 +80,7 @@ class TweetScraperPro:
                 await output.Tweets(tweet, "", self.config, self.conn)
 
     async def tweets(self):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+tweets')
         await self.Feed()
         if self.config.Location:
             self.count += await get.Multi(self.feed, self.config, self.conn)
@@ -82,6 +90,7 @@ class TweetScraperPro:
                 await output.Tweets(tweet, "", self.config, self.conn)
 
     async def main(self):
+        #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+main')
         if self.config.User_id is not None:
             self.config.Username = await get.Username(self.config.User_id)
 
@@ -96,6 +105,7 @@ class TweetScraperPro:
                     self.d._until = self.d._until - _days
                     self.feed = [-1]
 
+                #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+main+CallingGetLimit1')
                 if get.Limit(self.config.Limit, self.count):
                     self.d._until = self.d._until - _days
                     self.feed = [-1]
@@ -113,6 +123,7 @@ class TweetScraperPro:
                 else:
                     break
 
+                #loggin.info("[<] " + str(datetime.now()) + ':: run+TweetScraperPro+main+CallingGetLimit2')
                 if get.Limit(self.config.Limit, self.count):
                     break
 
@@ -120,13 +131,16 @@ class TweetScraperPro:
             verbose.Count(self.count, self.config)
 
 def run(config):
+    #loggin.info("[<] " + str(datetime.now()) + ':: run+run')
     get_event_loop().run_until_complete(TweetScraperPro(config).main())
 
 def Favorites(config):
+    #loggin.info("[<] " + str(datetime.now()) + ':: run+Favorites')
     config.Favorites = True
     run(config)
 
 def Followers(config):
+    #loggin.info("[<] " + str(datetime.now()) + ':: run+Followers')
     output.clean_follow_list()
     config.Followers = True
     config.Following = False
@@ -138,6 +152,7 @@ def Followers(config):
     storage.panda.clean()
 
 def Following(config):
+    #loggin.info("[<] " + str(datetime.now()) + ':: run+Following')
     output.clean_follow_list()
     config.Following = True
     config.Followers = False
@@ -149,10 +164,12 @@ def Following(config):
     storage.panda.clean()
 
 def Profile(config):
+    config.Profile = True
+    #loggin.info("[<] " + str(datetime.now()) + ':: run+Profile')
     run(config)
 
-
 def Search(config):
+    #loggin.info("[<] " + str(datetime.now()) + ':: run+Search')
     config.TwitterSearch = True
     config.Following = False
     config.Followers = False
